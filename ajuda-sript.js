65c904f0b7d29296cc06aaf403bf072301cc0321
joana-zoeh <136838992+joana-zoeh@users.noreply.github.com>
2023-12-05 19:39:00 -0300

    Add files via upload

---
+++ ajuda-sript.js
@@ -0,0 +1,10 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "ÍCONES/menu_icon(1).png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "ÍCONES/fechar_icon.svg";
+    }
+}
